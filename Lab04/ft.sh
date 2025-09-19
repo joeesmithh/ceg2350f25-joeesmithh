89d885d93e2aa2a7eaae4f19f0e81bbd2188ecb6
joeesmithh <josmth3030@gmail.com>
2025-09-19 17:40:12 -0400

    feat(Lab04): implement validate-record function to validate 2nd arg form

--- Lab04/ft.sh
+++ Lab04/ft.sh
@@ -2,9 +2,54 @@
 
 # ----------------- FUNCTION DEFS ----------------- #
 
+# Returns the user 'guide' string
+user-guide () {
+	local guide="This is the user guide."
+	echo "$guide" # Send guide string to stdout
+}
+
+
+
+
+
+
+# McDonalds 32.95
+# McDonalds 32
+# McDonalds 0.95
+# McDonalds -32.95
+
+
+
+
+
+
+
+
+
+# Validates and returns the user's second record argument
+arg2=$2
+validate-record () {
+
+	local record=$arg2
+
+	# Loop while record is either empty or not of correct form.
+	#
+	# To validate record argument of form "RECORD {-}00{.00}":
+	# 			.*" "+  # Match one or more character before one or more space character
+	# 			   \-?	# Negative sign optional
+	# 	   [0-9][1-9]*  # Match zero or more characters between 1-9 after a character 0-9
+	# (\.[0-9][1-9]*)?  # If optional decimal input, match zero or more 1-9 after 0-9
+	while [[ -z $record || ! $record =~ ^.+" "+\-?[0-9][1-9]*(\.[0-9][1-9]*)?$ ]]; do
+		read -p "Invalid record! Try again: " record
+	done
+	
+	echo "$record"
+}
+
 # Add a record to finances
 add () {
-	return 1
+	record="$(validate-record)"
+	echo "$record"
 }
 
 # Remove a record to finances
@@ -20,12 +65,6 @@ view () {
 # Clear all finance records
 clear () {
 	return 4
-}
-
-# Returns the user 'guide' string
-user-guide () {
-	local guide="This is the user guide."
-	echo "$guide" # Send guide string to stdout
 }
 
 # Output user guide to console
